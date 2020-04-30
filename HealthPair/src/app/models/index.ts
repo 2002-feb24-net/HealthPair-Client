92c2475b8b3e790ce0d5be281fb428aa847f29fb
Kyran <k.b_bryant@icloud.com>
2020-04-30 09:50:28 -0500

    Dockerization Station

---
+++ HealthPair/src/app/models/index.ts
@@ -0,0 +1,6 @@
+export * from './appointment';
+export * from './facility';
+export * from './insurance';
+export * from './patient';
+export * from './provider';
+export * from './specialty';
--- HealthPair/src/environments/environment.prod.ts
+++ HealthPair/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  healthPairApiBaseUrl: 'https://healthpair-api.azurewebsites.net'
 };
