33103947a1e4bd27805d8630b9cf2260501aa869
Tyler <claypooltyler00@gmail.com>
2020-05-01 15:26:01 -0500

    Added getLocation to user loc service

--- HealthPair/src/app/user-location.service.ts
+++ HealthPair/src/app/user-location.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserLocationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLocation() {
+    return this.http.get('https://ipapi.co/{format}')
+  }
 }
