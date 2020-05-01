48fbd7f86c52ee45bdad3d0a5f13f3e01a6f028a
Tyler <claypooltyler00@gmail.com>
2020-05-01 15:38:42 -0500

    Added getLocation to component

--- HealthPair/src/app/landing-page/landing-page.component.ts
+++ HealthPair/src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { UserLocationService } from '../user-location.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,14 +11,27 @@ import { FormControl } from '@angular/forms';
 export class LandingPageComponent implements OnInit {
   insurance: string;
   specialty: string;
+
+  //Get longitude and latitude
+  lat:string = '';
+  long:string = '';
+
+  //Hold an instance of the user-loaction service
+  location: Object;
   
   landingpageForm = this.builder.group({
     insurance: ['Humana'],
     specialty: ['Optometry']
   })
-  constructor(private builder: FormBuilder) { }
+  constructor(private builder: FormBuilder, private userLocation: UserLocationService) { }
 
   ngOnInit(): void {
+    //Calls the servce when it the component loads
+    this.userLocation.getLocation().subscribe(data => {
+      console.log(data);
+      this.lat = data.latitude;
+      this.long = data.longitude;
+    })
   }
 
   onSubmit() {
