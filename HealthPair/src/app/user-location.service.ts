ad2e14ecab195c865134b5d57dc15aa94b5b098b
Tyler <claypooltyler00@gmail.com>
2020-05-01 16:46:45 -0500

    able to get user location

--- HealthPair/src/app/user-location.service.ts
+++ HealthPair/src/app/user-location.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
+//Cerate an interface for location to store longitude nd latitude
+interface Location {
+  latitude:string;
+  longitude:string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +15,6 @@ export class UserLocationService {
   constructor(private http: HttpClient) { }
 
   getLocation() {
-    return this.http.get('https://ipapi.co/{format}')
+    return this.http.get<Location>('https://ipapi.co/json/')
   }
 }
