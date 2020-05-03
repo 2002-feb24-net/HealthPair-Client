abeddca923caaf48634e621b81194489c48525f5
Paul Edwards <pauldedwards95@gmail.com>
2020-05-02 22:03:24 -0500

    fixed login bug

--- HealthPair/src/app/app.module.ts
+++ HealthPair/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormControl } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { ProviderSelectionComponent } from './provider-selection/provider-selection.component';
 import { AppointmentDetailsComponent } from './appointment-details/appointment-details.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -22,6 +25,7 @@ import { InsuranceDetailsComponent } from './insurance-details/insurance-details
 import { AlertComponent } from './alert/alert.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { JwtInterceptor, ErrorInterceptor } from './authentification';
 
 
 
@@ -52,9 +56,13 @@ import { MaterialModule } from './material-module';
     MDBBootstrapModule.forRoot(),
     BrowserAnimationsModule,
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
