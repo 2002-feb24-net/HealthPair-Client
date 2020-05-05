46853249b7b3dbffb4ea991d58ea7e0de022a664
Kyran <k.b_bryant@icloud.com>
2020-05-04 22:13:32 -0700

    Register Page Template, still need to actually send request to API.

--- HealthPair/src/app/models/index.ts
+++ HealthPair/src/app/models/index.ts
@@ -1,6 +1,6 @@
 export * from './appointment';
 export * from './facility';
 export * from './insurance';
-export * from './patient';
+export * from './Patient';
 export * from './provider';
 export * from './specialty';
