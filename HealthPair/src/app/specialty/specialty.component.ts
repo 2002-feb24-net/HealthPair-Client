6138e8377d0f0fc84a8ad1c2bf8fa30d5c1b488f
Tyler <claypooltyler00@gmail.com>
2020-05-05 20:16:26 -0500

    Specialty can now be added with HTML

--- HealthPair/src/app/specialty/specialty.component.ts
+++ HealthPair/src/app/specialty/specialty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HealthPairService,AuthenticationService} from '../_services';
+import {Specialty, Patient} from '../models'
 
 @Component({
   selector: 'app-specialty',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./specialty.component.css']
 })
 export class SpecialtyComponent implements OnInit {
+  currentPatient : Patient;
+  specialty : Specialty;
+  mySpecialties : Specialty[];
+  createdMessage : string;
+  highest: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private HealthPairService: HealthPairService,private authenticationService: AuthenticationService) {
+    this.currentPatient = this.authenticationService.CurrentPatientValue;
+    // this.getHighestInsuranceId();
+    this.mySpecialties = [];
   }
 
+  ngOnInit() {
+  }
+
+  AddSpecialty(specialty: string) {
+    let newSpecialty = new Specialty
+    {
+      newSpecialty.Specialty = specialty
+    }
+    this.HealthPairService.createSpecialty(newSpecialty)
+    .subscribe(mySpecialty => {
+      this.mySpecialties.push(mySpecialty)
+    });
+    
+    this.createdMessage = "Your specialty has been created!"
+  }
+
+  delete(specialty: Specialty): void {
+    this.mySpecialties = this.mySpecialties.filter(r => r !== specialty);
+    this.HealthPairService.deleteFacility(specialty.SpecialtyId).subscribe();
+  }
+  getHighestSpecialtyId()
+  {
+    this.HealthPairService.getSpecialtyAll()
+      .subscribe(specialties => {
+        this.mySpecialties = specialties,
+        this.highest = this.mySpecialties[this.mySpecialties.length-1].SpecialtyId;
+      });
+  }
 }
