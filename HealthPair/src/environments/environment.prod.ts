d0c5930d56e6029aa3682014c20c2ee0bfa64c19
Paul Edwards <pauldedwards95@gmail.com>
2020-05-11 18:02:52 -0500

    deployment adds

--- HealthPair/src/environments/environment.prod.ts
+++ HealthPair/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  healthPairApiBaseUrl: 'https://healthpair-api.azurewebsites.net/',
-  //healthPairApiBaseUrl: 'a6ce75131bda94b61ab4b7f5f28aea65-578836505.us-east-2.elb.amazonaws.com/',
+  //healthPairApiBaseUrl: 'https://healthpair-api.azurewebsites.net/',
+  healthPairApiBaseUrl: 'https://healthpair-api.2002.revaturelabs.com',
   geoLocationBaseUrl: 'https://maps.googleapis.com/maps/api/geocode/json?address='
+  //notesApiBaseUrl: 'https://healthpair-api.2002.revaturelabs.com/'
 };
