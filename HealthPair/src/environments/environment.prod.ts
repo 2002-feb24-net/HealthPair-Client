744459c100b6cbd278070be0a4cc70bb4f27f746
Paul Edwards <pauldedwards95@gmail.com>
2020-05-11 11:10:45 -0500

    slight tweak, wont fix problem, but will help diagnose it

--- HealthPair/src/environments/environment.prod.ts
+++ HealthPair/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   healthPairApiBaseUrl: 'https://healthpair-api.azurewebsites.net/',
+  //healthPairApiBaseUrl: 'a6ce75131bda94b61ab4b7f5f28aea65-578836505.us-east-2.elb.amazonaws.com/',
   geoLocationBaseUrl: 'https://maps.googleapis.com/maps/api/geocode/json?address='
 };
--- HealthPair/src/environments/environment.ts
+++ HealthPair/src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment =
   // healthPairApiBaseUrl: 'http://192.168.99.100:8000/',
   //healthPairApiBaseUrl: 'https://localhost:44344/',
   healthPairApiBaseUrl: 'https://healthpair-api.azurewebsites.net/',
-  //healthPairApiBaseUrl: 'a6ce75131bda94b61ab4b7f5f28aea65-578836505.us-east-2.elb.amazonaws.com',
+  //healthPairApiBaseUrl: 'a6ce75131bda94b61ab4b7f5f28aea65-578836505.us-east-2.elb.amazonaws.com/',
   geoLocationBaseUrl: 'https://maps.googleapis.com/maps/api/geocode/json?address='
 };
